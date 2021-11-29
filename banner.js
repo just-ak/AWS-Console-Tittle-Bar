f2ce84141378b536117ce98ac72b88733e874c34
Andrew Kentigern-Fox <andrew@kentigern-fox.com>
2021-11-29 13:51:46 +0000

    1.9 Update

--- banner.js
+++ banner.js
@@ -1,72 +1,78 @@
-if (element1 = document.querySelectorAll('[data-testid="aws-my-account-details"]')[0]) {
-  element1 = document.querySelectorAll('[data-testid="aws-my-account-details"]')[0].innerHTML
-  accountId = element1;
+const baseElement = document.querySelector('[data-testid="account-detail-menu"]');
+
+let accountId;
+
+if (baseElement) {
+    const spanElement = baseElement.querySelectorAll('span')[1];
+
+    if (spanElement) {
+        accountId = spanElement.innerText.replace(/-/g, '');
+    }
+}
+if (accountId) {
+    getAccountName(accountId)
+        .then(accountDetails => {
+          console.log('accountId')
+            if (document.getElementById('awsconsolelables')) {
+                el = document.getElementById('awsconsolelables');
+            } else {
+                el = document.createElement('div');
+                el.id = 'awsconsolelables';
+                el.classList.add('awsconsolelables');
+                document.querySelectorAll('header')[0].querySelectorAll('nav')[0].prepend(el);
+                
+            }
+
+            if (document.getElementById('awsconsolelables_name')) {
+                elaccname = document.getElementById('awsconsolelables_name');
+            } else {
+                elaccname = document.createElement('div');
+                elaccname.id = 'awsconsolelables_name';
+                elaccname.classList.add('name');
+                el.appendChild(elaccname);
+                
+            }
+
+            if (accountDetails.id) {
+                elaccname.innerText = accountDetails.id;
+            } else {
+                elaccname.innerText = 'UnKnown';
+            }
+
+            if (accountDetails.color) {
+                elaccname.style.setProperty('color', accountDetails.color, 'important');
+            }
+
+            if (accountDetails.background) {
+                elaccname.style.background = accountDetails.background;
+            }
+
+            if (accountDetails.headerBackground) {
+                document.querySelectorAll('header')[0].style.background = accountDetails.headerBackground;
+            }
+
+            if (accountDetails.headerColor) {
+                document.querySelectorAll('[data-testid="awsc-nav-account-menu-button"]')[0].style.setProperty('color', accountDetails.headerColor, 'important');
+                document.querySelectorAll('[data-testid="awsc-nav-regions-menu-button"]')[0].style.setProperty('color', accountDetails.headerColor, 'important');
+                document.querySelectorAll('[data-testid="awsc-nav-support-menu-button"]')[0].style.setProperty('color', accountDetails.headerColor, 'important');
+                document.querySelectorAll('[data-testid="aws-services-list-button"]')[0].style.setProperty('color', accountDetails.headerColor, 'important');
+            }
+        })
+        .catch(e => console.error(e));
 } else {
-  element2 = document.querySelectorAll('[data-testid="account-menu-title"]')[1].parentElement.innerText.replace(/\D/g, '');
-  accountId = ""; //accounts[element2];     
+    console.warn('Unable to find Account Id');
 }
 
-getAccountName(accountId).then(accountDetails => {
-  if (document.getElementById("awsconsolelables")) {
-
-    el = document.getElementById("awsconsolelables")
-  } else {
-
-    el = document.createElement("div");
-    el.id = "awsconsolelables"
-    el.classList.add("awsconsolelables");
-    document.querySelectorAll('header')[0].appendChild(el)
-  }
-  if (document.getElementById("awsconsolelables_name")) {
-    
-    elaccname = document.getElementById("awsconsolelables_name")
-  } else {
-
-    elaccname = document.createElement("div");
-    elaccname.id = "awsconsolelables_name"
-    elaccname.classList.add("name");
-    el.appendChild(elaccname)
-  }
-  if (accountDetails[accountId])  {
-    if (accountDetails[accountId].id) {
-  
-    elaccname.innerText = accountDetails[accountId].id;
-    }
-    else {
-  
-      elaccname.innerText = "UnKnown";
-    }
-
-
-    if (accountDetails[accountId].color) {
-      elaccname.style.setProperty("color", accountDetails[accountId].color,"important")
-    }
-    if (accountDetails[accountId].background) {
-      elaccname.style.background = accountDetails[accountId].background;
-    }
-    if (accountDetails[accountId].headerBackground) {
-      document.querySelectorAll('header')[0].style.background = accountDetails[accountId].headerBackground;
-    }
-    if (accountDetails[accountId].headerColor) {
-      document.querySelectorAll('[data-testid="awsc-nav-account-menu-button"]')[0].style.setProperty("color", accountDetails[accountId].headerColor,"important")
-      document.querySelectorAll('[data-testid="awsc-nav-regions-menu-button"]')[0].style.setProperty("color", accountDetails[accountId].headerColor,"important")
-      document.querySelectorAll('[data-testid="awsc-nav-support-menu-button"]')[0].style.setProperty("color", accountDetails[accountId].headerColor,"important")
-      document.querySelectorAll('[data-testid="aws-services-list-button"]')[0].style.setProperty("color", accountDetails[accountId].headerColor,"important")
-  
-    }
-  } else  {
-
-  }
-
-})
-
 async function getAccountName(accountId) {
-  let promise = new Promise(function (resolve, reject) {
-    chrome.storage.local.get('jsoninfo', function (response) {
-      let accountDetails = JSON.parse(response.jsoninfo);
-      resolve(accountDetails);
-    })
-  });
-  let accountDetails = await promise
-  return accountDetails
+    return await new Promise((resolve, reject) =>
+        chrome.storage.local.get('jsoninfo', response => {
+            const accountDetails = JSON.parse(response.jsoninfo);
+
+            if (accountDetails && accountDetails.hasOwnProperty(accountId)) {
+                resolve(accountDetails[accountId]);
+            } else {
+                reject(`Unknown Account Id: ${accountId}`);
+            }
+        })
+    );
 }
