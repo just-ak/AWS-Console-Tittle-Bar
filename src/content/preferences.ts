1779f3976bfc196cc39b117116c61c1c99dea680
Andrew Kentigern-Fox <andrew@kentigern-fox.com>
2023-05-29 21:40:02 +0100

    feat: Change Colour Picker, and add URL managemnet to popup

---
+++ src/content/preferences.ts
@@ -0,0 +1,82 @@
+const { pr_getAllAccounts, pr_saveAllAccounts } = require('./reference');
+
+const jsonElementID = 'jsoninfo';
+const jsonAccElementID = 'jsonaccounts';
+
+interface AccountDetails {
+  id: string;
+  color: string;
+  background: string;
+  headerBackground: string;
+  headerColor: string;
+}
+
+function save_options() {
+  const jsonTextArea = document.getElementById(jsonElementID) as HTMLTextAreaElement;
+  const jsoninfo = jsonTextArea.value;
+  chrome.storage.local.set(
+    {
+      jsoninfo: jsoninfo,
+    },
+    function () {
+      const status = document.getElementById('status');
+      status.textContent = 'Options Extras saved.';
+      setTimeout(function () {
+        status.textContent = '';
+      }, 750);
+    }
+  );
+}
+
+function restore_options() {
+  chrome.storage.local.get(
+    {
+      jsoninfo: '{"urls":[{"url":"https://example.com","title":"Example.con"}]}',
+    },
+    function (options) {
+      const jsonTextArea = document.getElementById(jsonElementID) as HTMLTextAreaElement;
+      jsonTextArea.value = options.jsoninfo;
+    }
+  );
+  pr_getAllAccounts().then((data) => {
+    showAccounts(data);
+  });
+}
+
+// Retrieve JSON data from chrome.storage.local
+const showAccounts = (jsonData) => {
+  const container = document.getElementById('accountColors');
+  const form = document.createElement('form');
+  for (const accountId in jsonData) {
+    if (jsonData[accountId] !== undefined) {
+      console.log(accountId);
+      const account = jsonData[accountId];
+      console.log(JSON.stringify(account));
+      const accountDiv = document.createElement('div');
+      const nameLabel = document.createElement('label');
+      nameLabel.textContent = account.id + ': ' + accountId;
+
+      accountDiv.appendChild(nameLabel);
+      const colorInput = document.createElement('input');
+      colorInput.type = 'color';
+      colorInput.value = account.color;
+      colorInput.addEventListener('change', function (event) {
+        const selectedColor = (event.target as HTMLInputElement).value;
+        console.log(`Color : ${selectedColor}`);
+        const accountId = (event.target as HTMLInputElement).parentNode.querySelector('label').textContent.split(':')[1].trim();
+        console.log(`Acc : ${accountId}`);
+        const curList = pr_getAllAccounts().then((data) => {
+          data[accountId].color = selectedColor;
+          pr_saveAllAccounts(data);
+        });
+      });
+
+      accountDiv.appendChild(colorInput);
+      form.appendChild(accountDiv);
+    }
+  }
+  container.appendChild(form);
+};
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,37 @@
 const path = require('path');
+//const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+//const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: {
-    // options: './src/options.ts',
-    // popup:'./src/popup/choose_page.ts',
-    // banner: './src/banner.ts',
+    preferences: './src/content/preferences.ts',
+    plugin_page: './src/content/plugin_page.ts',
+    content_pages: './src/content/content_pages.ts',
   },
   output: {
-    // iife: false,
-    // // globalObject: 'this',
-    // libraryTarget: 'global', // or 'global' or 'commonjs2' or window
-    // path: path.resolve(__dirname, 'dist'),
-    // filename: (pathData) => {
-    //   if (pathData.chunk.name === 'options') {
-    //     return 'options.js';
-    //   } else if (pathData.chunk.name === 'banner') {
-    //     return 'banner.js';
-    //   } else if (pathData.chunk.name === 'popup') {
-    //     return 'popup/choose_page.js';
-    //   } else {
-    //     return '[name]/[name].js';
-    //   }
-    // },
+    path: path.resolve(__dirname, './dist/'),
+    filename: (pathData) => {
+      const entryName = pathData.chunk.name;
+      return `content/${entryName}_bundle.js`;
+    },
   },
   optimization: {
-    minimize: false
+    minimize: false,
   },
+
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        use: 'html-loader'
+        use: 'html-loader',
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
@@ -49,31 +42,34 @@ module.exports = {
           publicPath: 'images',
         },
       },
-    ]
+    ],
   },
   resolve: {
     alias: {
       browser: path.resolve(__dirname, 'src'),
     },
-    extensions: ['.tsx', '.tJs', '.js']
+    extensions: ['.tsx', '.tJs', '.js'],
   },
-
-  //devtool: 'source-map',
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   template: 'src/options.html',
-    //   filename: 'options.html',
-    //   chunks: ['options']
-    // }),
+    new HtmlWebpackPlugin({
+      template: 'src/content/plugin_page.html',
+      filename: 'content/plugin_page.html',
+      chunks: ['plugin_page'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/content/preferences.html',
+      filename: 'content/preferences.html',
+      chunks: ['preferences'],
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['content_pages'],
+    }),
     new CopyWebpackPlugin({
       patterns: [
         { from: 'src/manifest.json', to: 'manifest.json' },
-        { from: 'src/options.html', to: 'options.html' },
-        { from: 'src/awsconsolestyle.css', to: 'awsconsolestyle.css' },
+        { from: 'src/content/content_pages.css', to: 'content/content_pages.css' },
         { from: 'src/icons', to: 'icons' },
-        { from: 'src/popup/*.html', to:'popup/[name][ext]' },
-        { from: 'src/popup/*.css', to:'popup/[name][ext]' },
-      ]
-    })
-  ]
+      ],
+    }),
+  ],
 };
