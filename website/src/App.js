f057d4e71e9dfcb25a71f23b657a03555317b380
Andrew Kentigern-Fox <andrew@kentigern-fox.com>
2024-11-17 12:51:23 +0000

    feat: Advertising Website

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import awsLogo from './assets/aws-logo.png'; // Add your AWS logo image here
 
