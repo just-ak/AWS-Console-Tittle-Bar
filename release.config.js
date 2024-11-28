c4645ca2b8f0b082435e0370b93643314f9ddb29
Andrew Kentigern-Fox <3976536+just-ak@users.noreply.github.com>
2024-11-28 21:34:37 +0000

    fix: Release is not working correctly

---
+++ release.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalCommits",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        releaseRules: [
+          { type: "chore", scope: "deps", release: "patch" }
+        ]
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalCommits",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        presetConfig: {
+          types: [
+            { type: "feat", section: "Features" },
+            { type: "fix", section: "Bug Fixes" },
+            { type: "perf", section: "Performance Improvements" },
+            { type: "revert", section: "Reverts" },
+            { type: "docs", section: "Documentation", hidden: false },
+            { type: "style", section: "Styles", hidden: false },
+            { type: "chore", section: "Dependency Updates", hidden: false },
+            { type: "refactor", section: "Code Refactors", hidden: false },
+            { type: "test", section: "Tests", hidden: false },
+            { type: "build", section: "Build System", hidden: false },
+            { type: "ci", section: "CI/CD", hidden: false },
+            { type: "improvement", section: "Improvements", hidden: false }
+          ]
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"]
+        }
+      }
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "archive/*.zip", label: "Distribution ZIP" }
+        ]
+      }
+    ],
+    "@semantic-release/github"
+  ]
+};
